5c6dc8516669f9f9c3a498be1e86ae4d60b962d0
PS-Ddevil <sinhapurushottam911@gmail.com>
2020-02-17 14:23:25 +0530

    Updates

---
+++ precache-manifest.12c0508117c3b7e428b69a621f124f26.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3867e3d0ddd94d757d1c6ab170230e35",
+    "url": "/index.html"
+  },
+  {
+    "revision": "95cda1c1351bd08d54da",
+    "url": "/static/css/2.b6a8f24c.chunk.css"
+  },
+  {
+    "revision": "d6cfbf6e283d72d9b359",
+    "url": "/static/css/main.6684347e.chunk.css"
+  },
+  {
+    "revision": "95cda1c1351bd08d54da",
+    "url": "/static/js/2.64dd109c.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/static/js/2.64dd109c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6cfbf6e283d72d9b359",
+    "url": "/static/js/main.9dcf57f9.chunk.js"
+  },
+  {
+    "revision": "702d5c2880dd268b72be",
+    "url": "/static/js/runtime-main.453d516c.js"
+  },
+  {
+    "revision": "79308f43f0533137e8e0c52cbab6c03c",
+    "url": "/static/media/QR.79308f43.png"
+  },
+  {
+    "revision": "5a93f4e0577509023f09a42c8f211b42",
+    "url": "/static/media/amit.5a93f4e0.jpg"
+  },
+  {
+    "revision": "dfb461b8d7a70cdd3585eef37f393fc3",
+    "url": "/static/media/amitjr.dfb461b8.jpg"
+  },
+  {
+    "revision": "207285ba2978b2afe7da010c216d80af",
+    "url": "/static/media/ankit.207285ba.jpg"
+  },
+  {
+    "revision": "4abd2a5a3c2b9dbdd2307a0bdaeb29c1",
+    "url": "/static/media/autocad.4abd2a5a.png"
+  },
+  {
+    "revision": "fda9ffc24933cd020c9488b8dc669afa",
+    "url": "/static/media/bg10.fda9ffc2.jpg"
+  },
+  {
+    "revision": "3c69350a113742d7fd0a0567bfb0e85e",
+    "url": "/static/media/bg2.3c69350a.jpg"
+  },
+  {
+    "revision": "48d36da4c1555befbc58aca6a18db0f3",
+    "url": "/static/media/bridge.48d36da4.jpg"
+  },
+  {
+    "revision": "9ee7fd82b6db9d4da58cfdb757511261",
+    "url": "/static/media/dangi.9ee7fd82.jpg"
+  },
+  {
+    "revision": "5560b36656a4d600ec7a19cbd490849c",
+    "url": "/static/media/garva.5560b366.jpg"
+  },
+  {
+    "revision": "0175bc7f96a597c5588fcd7024f727bb",
+    "url": "/static/media/iit_mandi.0175bc7f.png"
+  },
+  {
+    "revision": "8b81550bebfb56ce1add64d8a922b837",
+    "url": "/static/media/mahipal.8b81550b.jpg"
+  },
+  {
+    "revision": "469ba028e4f3ea7efc9106bac4671f84",
+    "url": "/static/media/nirmaan.469ba028.png"
+  },
+  {
+    "revision": "7c3f606d72473cca5f65516922dadfcc",
+    "url": "/static/media/puru.7c3f606d.jpg"
+  },
+  {
+    "revision": "23cb2bbb9e8150d5e3db15626267b6b2",
+    "url": "/static/media/raj.23cb2bbb.jpg"
+  },
+  {
+    "revision": "00a84bd6cfd745a6e12f1c333cf311b1",
+    "url": "/static/media/sanjeev.00a84bd6.jpg"
+  },
+  {
+    "revision": "e1f37b347b6d213f69e6c659089edc92",
+    "url": "/static/media/shivam.e1f37b34.jpg"
+  },
+  {
+    "revision": "874730772fbcb6e37967497e3bb1791a",
+    "url": "/static/media/vikas.87473077.jpg"
+  },
+  {
+    "revision": "2ede115366fee1bb0a9cdf7cb597b631",
+    "url": "/static/media/vikash.2ede1153.jpg"
+  }
+]);
